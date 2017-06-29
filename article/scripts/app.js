3fe922547f231835b2f93c4a1568c6301d5a2538
Jason Li <jasonli2310@gmail.com>
2017-06-29 15:48:19 -0500

    data passes through form 1

--- article/scripts/app.js
+++ article/scripts/app.js
@@ -2,11 +2,27 @@
 var vm = new Vue({
   el: '#copy',
   data: {
-    isActive: false
+    answer: {story: "", question: "", decision: "", why: ""},
+
+    isActive: false,
+
+    answers: [
+      {story: "1", question: "1", decision: "agree", why: "because I like it"},
+      {story: "1", question: "2", decision: "agree", why: "because I love it"},
+
+      {story: "1", question: "3", decision: "disagree", why: "because I hate it"}
+    ]
+
   },
   methods: {
     change: function(){
       this.isActive = !this.isActive;
+    },
+
+    addAnswer: function () {
+      this.answers.push(this.answer);
+      this.answer = {story: "", question: "", decision: "", why: ""};
+
     }
   }
 });
