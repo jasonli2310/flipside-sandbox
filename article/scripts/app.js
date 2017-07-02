a00cbf7bb4b320e997fbe886da96277b22348841
Jason Li <jasonli2310@gmail.com>
2017-07-02 15:22:04 -0500

    fixed forms

--- article/scripts/app.js
+++ article/scripts/app.js
@@ -1,27 +1,32 @@
 //app javascript
+
+
+
+
+
+
 var vm = new Vue({
   el: '#copy',
   data: {
-    answer: {story: "", question: "", decision: "", why: ""},
+    answer: {question: "", decision: "", why: ""},
 
-    isActive: false,
+    isActive: [false, false],
 
     answers: [
-      {story: "1", question: "1", decision: "agree", why: "because I like it"},
-      {story: "1", question: "2", decision: "agree", why: "because I love it"},
-
-      {story: "1", question: "3", decision: "disagree", why: "because I hate it"}
+      {question: "1", decision: "agree", why: "because I like it"},
+      {question: "2", decision: "agree", why: "because I love it"},
+      {question: "3", decision: "disagree", why: "because I hate it"}
     ]
 
   },
   methods: {
-    change: function(){
-      this.isActive = !this.isActive;
+    changeit: function(index){
+      this.isActive[index] = !this.isActive[index];
     },
-
-    addAnswer: function () {
+    addAnswer: function (input) {
+      this.answer.question = input;
       this.answers.push(this.answer);
-      this.answer = {story: "", question: "", decision: "", why: ""};
+      this.answer = {question: "", decision: "", why: ""};
 
     }
   }
