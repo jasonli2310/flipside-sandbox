e84f96cdba4348159fd8566e8e5caa89cd6bf627
Jason Li <jasonli2310@gmail.com>
2017-07-03 14:22:49 -0500

    charting done

---
+++ article/formatting.py
@@ -0,0 +1,46 @@
+# Flipside Test Data for Newsletter 2
+#
+# Jason Li - 7/2/17
+#
+#
+#
+import sys
+#import numpy as np
+import csv
+import math
+#############################
+#  defining methods         #
+#############################
+
+#####
+#######
+#########
+###########
+
+
+
+f = open('testdata.csv', 'rb')
+a = f.readlines()
+for a{
+
+}
+
+a[2].strip().split(",")[:4]
+
+print (a)
+
+
+{
+label: 'Scatter Dataset',
+backgroundColor: 'blue',
+data: [{
+x: -10,
+y: 0.5
+}, {
+x: 0,
+y: 9.6
+}, {
+x: 10,
+y: 5
+}]
+}
