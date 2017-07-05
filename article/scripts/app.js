bc149aad745160f20562d6a2fc67f27af128d549
Jason Li <jasonli2310@gmail.com>
2017-07-05 18:03:49 -0500

    done with option displaying under chart
    woohoo

--- article/scripts/app.js
+++ article/scripts/app.js
@@ -1,10 +1,5 @@
 //app javascript
 
-
-
-
-
-
 var vm = new Vue({
   el: '#copy',
   data: {
@@ -34,4 +29,30 @@ var vm = new Vue({
       this.decided = true;
     }
   }
+});
+
+
+var results = new Vue ({
+  el: '#opresult',
+  data: {
+    groupkey: {group: '', opinion1: '', opinion2: '', opinion3: ''},
+
+    showing: '',
+
+    group: [
+      {name: "A", opinion1: 'I like puppies', opinion2: 'I like dogs', opinion3: 'i like cats'},
+      {name: "B", opinion1: 'I like SIDD', opinion2: 'I like FORREST', opinion3: 'i like JASON'},
+      {name: "C", opinion1: 'I like STOP', opinion2: 'I like LOOKING', opinion3: 'i like AT'},
+      {name: "D", opinion1: 'I like ME', opinion2: 'I like WHAT', opinion3: 'i like YOO'}
+    ],
+
+  },
+
+  methods: {
+    select: function(event) {
+      targetId = event.currentTarget.id;
+      this.showing = targetId;
+    }
+  }
+
 });
