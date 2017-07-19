7b96cebabce957f607658ccf5e9ab5e50066aae0
Jason Li <jasonli2310@gmail.com>
2017-07-19 19:22:24 -0400

    copied over the d3 stuff

--- article/scripts/app.js
+++ article/scripts/app.js
@@ -41,7 +41,7 @@ var vm = new Vue({
       currentid = event.currentTarget.id;
       this.why.question = currentid;
       this.whys.push(this.why);
-      this.why = {question: "", decision: ""};
+      this.why = {question: "", response: ""};
     }
   }
 });
@@ -73,114 +73,8 @@ var results = new Vue ({
   }
 });
 
-
-var ctx = document.getElementById("scatterPlot").getContext('2d');
-
-var userdata = [{"x": 0.3489973308078653, "y": 2.8484390551697265, "r": 9}]
-
-var dataset = [{"x": 1.4201333112919208, "y": 4.637461654822657, "r": 5}, {"x": 1.2893377801973964, "y": 3.449691588146665, "r": 5}, {"x": 1.7373078036934886, "y": 4.425462343941218, "r": 5}, {"x": 1.0220285958479411, "y": 4.116603477422556, "r": 5}, {"x": 1.8407062774060008, "y": 3.561622307041948, "r": 5}, {"x": -1.5767197372875836, "y": 4.957405922888751, "r": 5}, {"x": 2.4703491517041014, "y": 4.098629063682589, "r": 5}, {"x": 0.8730512267529372, "y": 4.714385829386762, "r": 5}, {"x": 3.2460247025341027, "y": 2.8494216528496255, "r": 5}, {"x": 1.1203136497073731, "y": 5.758060834411365, "r": 5}];
-
-var dataset2 = [{"x": 1.5456153396812244, "y": 0.45958935832675046, "r": 5}, {"x": 3.5880467357913357, "y": 2.367022429838222, "r": 5}, {"x": 1.1674817738027652, "y": -1.0831328082859901, "r": 5}, {"x": 1.6679407040249257, "y": 0.5953609093626013, "r": 5}, {"x": 0.8024721613829517, "y": 1.6751540157698468, "r": 5}, {"x": 3.2855582021605985, "y": 2.100043508722348, "r": 5}, {"x": 1.0067145563657853, "y": -0.5223542772410383, "r": 5}, {"x": 1.7073553721067254, "y": 1.054012629041917, "r": 5}, {"x": 2.2102149471297943, "y": 1.2758261795401105, "r": 5}];
-
-var dataset3 = [{"x": -2.3400502952663595, "y": 1.191599659001446, "r": 5}, {"x": -3.140801860779857, "y": 2.705141981119154, "r": 5}, {"x": -1.5550862415605602, "y": 3.3462141318635403, "r": 5}, {"x": -2.1993644609978564, "y": 2.5583290997925814, "r": 5}, {"x": -1.3494778709681525, "y": 2.5161013251248607, "r": 5}, {"x": -2.161226106902869, "y": 2.5551410953459848, "r": 5}, {"x": -2.422370574415581, "y": 3.3047847591923847, "r": 5}, {"x": -3.1571023601879498, "y": 3.380664516858897, "r": 5}, {"x": -1.4603867908387376, "y": 3.220354159072904, "r": 5}, {"x": -2.0377091507907785, "y": 1.7372500772107107, "r": 5}];
-
-var dataset4 = [{"x": -0.5191652125686134, "y": 7.964442926399005, "r": 5}, {"x": -0.8459141335686012, "y": 7.1506499247012805, "r": 5}, {"x": -2.4134056155295065, "y": 8.736286502595782, "r": 5}, {"x": -2.119052923928032, "y": 7.256610350877178, "r": 5}, {"x": -0.7825933350156904, "y": 6.29921632936437, "r": 5}, {"x": -0.10885509020284956, "y": 6.600634195287942, "r": 5}, {"x": 0.23999641904944946, "y": 9.731349191672177, "r": 5}, {"x": -2.1555541391293924, "y": 7.887405411437733, "r": 5}, {"x": -1.559808306873523, "y": 7.891625357871339, "r": 5}, {"x": -0.06947620358649953, "y": 7.6555351798292435, "r": 5}];
-
-
 var inputs = [userdata, dataset, dataset2, dataset3];
 
 var colors = ['#DB6061', '#587A95', '#52AEFB', '#85E785', '#d098f0', '#f67dd3'];
 
 var labels = ['A', 'B', 'C', 'D', 'E', 'F'];
-
-/*
-class Rectangle {
-  constructor(input, colors, labels) {
-    this.inputs = inputs;
-    this.colors = colors;
-    this.labels = labels;
-  }
-  var chartdataset = ;
-  var dict = { label: '', backgroundColor: '', data: ''};
-
-  produceChartData() {
-
-    for (var i = 1; i < this.inputs.length; i++) {
-      this.dict["label"] = this.labels[i];
-      this.dict.["backgroundColor"] = this.colors[i];
-      this.dict.["data"] = this.inputs[i];
-
-      this.chartdataset.push(this.dict);
-    }
-  }
-}
-*/
-
-var scatterChart = new Chart(ctx, {
-  type: 'bubble',
-  fill: false,
-  data: {
-    fill: false,
-
-    datasets: [
-      {
-        label: 'Scatter Dataset',
-        backgroundColor: 'purple',
-        data: userdata
-      },
-      {
-        label: 'Scatter Dataset',
-        backgroundColor: '#DB6061',
-        data: dataset
-      },
-
-      {
-        label: 'Scatter Dataset',
-        backgroundColor: '#587A95',
-        data: dataset2
-      },
-      {
-        label: 'Scatter Dataset',
-        backgroundColor: '#52aefb',
-        data: dataset3
-      },
-
-      {
-        label: 'Scatter Dataset',
-        backgroundColor: '#85E785',
-        data: dataset4
-      }
-    ]
-    },
-    options: {
-      legend: {
-        display: false
-      },
-      scales: {
-        yAxes:[{
-          gridLines: {
-            display:false
-          },
-          ticks: {
-            // Include a dollar sign in the ticks
-            callback: function() {
-              return '';
-            }
-          }
-        }],
-        xAxes: [
-          {gridLines: {
-            display:false
-          },
-          display: true,
-          type: 'linear',
-          ticks: {
-            // Include a dollar sign in the ticks
-            callback: function() {
-              return '';
-            }
-          }
-        }]
-      }
-    }
-  });
